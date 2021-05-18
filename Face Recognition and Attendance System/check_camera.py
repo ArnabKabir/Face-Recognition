bd50bc36d6f24dfacf65ce0eaf98125cfd2f83b3
Arnab <82705018+ArnabKabir@users.noreply.github.com>
2021-05-18 22:30:48 +0600

    Update check_camera.py

--- Face Recognition and Attendance System/check_camera.py
+++ Face Recognition and Attendance System/check_camera.py
@@ -2,17 +2,13 @@
 def camer():
     import cv2
 
-    # Load the cascade
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    # To capture video from webcam.
     cap = cv2.VideoCapture(0)
 
     while True:
-        # Read the frame
         _, img = cap.read()
 
-        # Convert to grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Detect the faces
@@ -26,10 +22,8 @@ def camer():
         # Display
         cv2.imshow('Webcam Check', img)
 
-        # Stop if escape key is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release the VideoCapture object
     cap.release()
     cv2.destroyAllWindows()
