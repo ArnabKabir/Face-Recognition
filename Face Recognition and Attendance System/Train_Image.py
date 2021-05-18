c14d8a3a492030eadf174bf1d5b5b5de9651ae29
Arnab <82705018+ArnabKabir@users.noreply.github.com>
2021-05-18 22:27:42 +0600

    Update Train_Image.py

--- Face Recognition and Attendance System/Train_Image.py
+++ Face Recognition and Attendance System/Train_Image.py
@@ -13,15 +13,10 @@ def getImagesAndLabels(path):
     imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
     faces = []
     Ids = []
-    # now looping through all the image paths and loading the Ids and the images
     for imagePath in imagePaths:
-        # loading the image and converting it to gray scale
         pilImage = Image.open(imagePath).convert('L')
-        # Now we are converting the PIL image into numpy array
         imageNp = np.array(pilImage, 'uint8')
-        # getting the Id from the image
         Id = int(os.path.split(imagePath)[-1].split(".")[1])
-        # extract the face from the training image sample
         faces.append(imageNp)
         Ids.append(Id)
     return faces, Ids
